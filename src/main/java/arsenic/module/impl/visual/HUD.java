a5716a6878c50c2d015f5c121b43ebbca54be123
K-v <58765519+K-ov@users.noreply.github.com>
2022-10-23 17:21:21 +1100

    ahudsabd

--- src/main/java/arsenic/module/impl/visual/HUD.java
+++ src/main/java/arsenic/module/impl/visual/HUD.java
@@ -1,28 +1,27 @@
 package arsenic.module.impl.visual;
 
+import org.lwjgl.input.Keyboard;
+
+import arsenic.event.bus.Listener;
+import arsenic.event.bus.annotations.EventLink;
 import arsenic.event.impl.EventRender2D;
 import arsenic.main.Arsenic;
 import arsenic.module.Module;
 import arsenic.module.ModuleCategory;
 import arsenic.module.ModuleInfo;
-import arsenic.utils.font.TTFontRenderer;
 import arsenic.utils.interfaces.IFontRenderer;
-import arsenic.event.bus.Listener;
-import arsenic.event.bus.annotations.EventLink;
-import org.lwjgl.input.Keyboard;
 
 @ModuleInfo(name = "HUD", category = ModuleCategory.VISUAL, keybind = Keyboard.KEY_Y)
 public class HUD extends Module {
 
     @EventLink
     public final Listener<EventRender2D> onRender2D = event -> {
-
         IFontRenderer mcFr = (IFontRenderer) mc.fontRendererObj;
-        TTFontRenderer ttfFr = Arsenic.getInstance().getFonts().FR;
-
-        mcFr.drawStringWithShadow("Test123", 4, 4, -1);
-        ttfFr.drawStringWithShadow("Test123", 4, 16, -1);
-
+        float yOffSet= 0;
+        for(Module m : Arsenic.getArsenic().getModuleManager().getEnabledModules()) {
+            mcFr.drawString(m.getName(), 0, yOffSet, 0xFFFFFFFF);
+            yOffSet += mcFr.getHeight(m.getName());
+        }
     };
 
     @Override
