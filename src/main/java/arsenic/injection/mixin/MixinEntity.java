cf57f5c10ff881c6b27c0f645f9227566395e41f
sigmaclientwastaken <blockmbot.2@gmail.com>
2022-11-06 20:05:26 +0100

    a

--- src/main/java/arsenic/injection/mixin/MixinEntity.java
+++ src/main/java/arsenic/injection/mixin/MixinEntity.java
@@ -1,42 +1,36 @@
 package arsenic.injection.mixin;
 
+import arsenic.event.impl.EventMove;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
-
-import arsenic.event.impl.EventMove;
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.Entity;
 
 @Mixin(Entity.class)
 public abstract class MixinEntity{
-
-
 
     @Shadow
     public float rotationYaw;
 
     public float cachedYaw;
 
-
-    @ModifyArgs(at = @At(value = "INVOKE"), method = "moveFlying")
-    private void moveFlying(Args args) {
+    @Inject(method = "moveFlying", at = @At("HEAD"))
+    private void moveFlyingHead(float p_moveFlying_1_, float p_moveFlying_2_, float p_moveFlying_3_, CallbackInfo ci) {
        if((Object) this == Minecraft.getMinecraft().thePlayer) {
-            EventMove e = new EventMove(args.get(0), args.get(1), args.get(3), rotationYaw);
-            args.set(0, e.getStrafe());
-            args.set(1, e.getForward());
-            args.set(2, e.getFriction());
+            EventMove e = new EventMove(p_moveFlying_1_, p_moveFlying_2_, p_moveFlying_3_, rotationYaw);
+            p_moveFlying_1_ = e.getStrafe();
+            p_moveFlying_2_ = e.getForward();
+            p_moveFlying_3_ = e.getFriction();
             this.rotationYaw = e.getYaw();
             cachedYaw = e.getYaw();
         }
     }
 
     @Inject(method = "moveFlying", at = @At("RETURN"))
-    public void moveFlying(float strafe, float forward, float friction, CallbackInfo ci) {
+    public void moveFlyingReturn(float strafe, float forward, float friction, CallbackInfo ci) {
         if((Object) this == Minecraft.getMinecraft().thePlayer)
             rotationYaw = cachedYaw;
     }
