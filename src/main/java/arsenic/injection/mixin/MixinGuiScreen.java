793305a38863d466ffb12cb396efebcae441f751
K-v <58765519+K-ov@users.noreply.github.com>
2023-02-04 06:45:46 +1100

    commands are now saved in chat

--- src/main/java/arsenic/injection/mixin/MixinGuiScreen.java
+++ src/main/java/arsenic/injection/mixin/MixinGuiScreen.java
@@ -1,8 +1,10 @@
 package arsenic.injection.mixin;
 
 import arsenic.main.Arsenic;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,10 +12,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value = GuiScreen.class)
 public class MixinGuiScreen {
 
+    @Shadow Minecraft mc;
+
     @Inject(method = "sendChatMessage(Ljava/lang/String;Z)V", at = @At(value = "HEAD"), cancellable = true)
     public void sendChatMessage(String msg, boolean addToChat, CallbackInfo ci) {
         if(msg.startsWith(".")) {
             Arsenic.getInstance().getCommandManagerManager().executeCommand(msg);
+            mc.ingameGUI.getChatGUI().addToSentMessages(msg);
             ci.cancel();
         }
     }
