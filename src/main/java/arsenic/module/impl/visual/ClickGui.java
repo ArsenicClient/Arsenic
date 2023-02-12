11a4eee1413372b919a58aa563efd6201dfac0aa
K-v <58765519+K-ov@users.noreply.github.com>
2023-02-12 14:33:55 +1100

    removed spaces from module names

--- src/main/java/arsenic/module/impl/visual/ClickGui.java
+++ src/main/java/arsenic/module/impl/visual/ClickGui.java
@@ -14,7 +14,7 @@ import arsenic.module.property.impl.doubleProperty.DoubleValue;
 
 import java.lang.reflect.Field;
 
-@ModuleInfo(name = "Click GUI", category = ModuleCategory.VISUAL, hidden = true, keybind = Keyboard.KEY_RSHIFT)
+@ModuleInfo(name = "ClickGUI", category = ModuleCategory.VISUAL, hidden = true, keybind = Keyboard.KEY_RSHIFT)
 public class ClickGui extends Module {
 
     public final DoubleProperty expandTop = new DoubleProperty("Expand Top", new DoubleValue(2D, 0D, 3D, 0.5D));
