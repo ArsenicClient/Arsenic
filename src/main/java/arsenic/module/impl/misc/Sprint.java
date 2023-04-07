e23717ae164599b654a7765d88482a1c27d36dc9
KassuK1 <kasper.s.kinnunen@gmail.com>
2023-04-07 13:11:43 +0300

    sprintos

--- src/main/java/arsenic/module/impl/misc/Sprint.java
+++ src/main/java/arsenic/module/impl/misc/Sprint.java
@@ -1,36 +1,37 @@
 package arsenic.module.impl.misc;
 
 import arsenic.event.bus.Listener;
+import arsenic.event.bus.annotations.EventLink;
 import arsenic.event.impl.EventTick;
 import arsenic.module.Module;
 import arsenic.module.ModuleCategory;
 import arsenic.module.ModuleInfo;
 import arsenic.module.property.impl.EnumProperty;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.input.Keyboard;
 
 @ModuleInfo(name = "Sprint",category = ModuleCategory.OTHER, keybind = Keyboard.KEY_V)
 //KEY_V more like KV
 public class Sprint extends Module {
     public final EnumProperty<sMode> sprintMode = new EnumProperty<>("Mode: ",sMode.Legit);
-    @Mod.EventHandler
-    public void onTick() {
-    if (sprintMode.getValue().equals(sMode.Rage)) {
-        KeyBinding.setKeyBindState(mc.gameSettings.keyBindSprint.getKeyCode(), true);
-    }
-    if (sprintMode.getValue().equals(sMode.Omni)) {
-        if (!mc.thePlayer.isCollided)
-            if (mc.gameSettings.keyBindForward.isPressed() || mc.gameSettings.keyBindBack.isPressed()|| mc.gameSettings.keyBindRight.isPressed()||mc.gameSettings.keyBindLeft.isPressed()) {
+
+    @EventLink
+    public final Listener<EventTick> onTick = event -> {
+        if (sprintMode.getValue().equals(sMode.Rage)) {
+            KeyBinding.setKeyBindState(mc.gameSettings.keyBindSprint.getKeyCode(), true);
+        }
+        if (sprintMode.getValue().equals(sMode.Omni)) {
+            if (!mc.thePlayer.isCollided)
+                if (mc.gameSettings.keyBindForward.isPressed() || mc.gameSettings.keyBindBack.isPressed()|| mc.gameSettings.keyBindRight.isPressed()||mc.gameSettings.keyBindLeft.isPressed()) {
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindSprint.getKeyCode(), true);
+                }
+        }
+        if (sprintMode.getValue().equals(sMode.Legit)) {
+            if (mc.thePlayer.moveForward != 0) {
                 KeyBinding.setKeyBindState(mc.gameSettings.keyBindSprint.getKeyCode(), true);
             }
-    }
-    if (sprintMode.getValue().equals(sMode.Legit)) {
-        if (mc.thePlayer.moveForward != 0) {
-            Listener<EventTick> onKeyPress = event-> KeyBinding.setKeyBindState(mc.gameSettings.keyBindSprint.getKeyCode(), true);
         }
-    }
-    }
+    };
     public enum sMode {
         Legit,Rage,Omni
     }
