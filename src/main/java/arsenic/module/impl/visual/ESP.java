d7336776c2d7129a121618d70f1fdfb30a570f2c
K-v <58765519+K-ov@users.noreply.github.com>
2023-04-10 06:18:15 +1000

    esp finally works

--- src/main/java/arsenic/module/impl/visual/ESP.java
+++ src/main/java/arsenic/module/impl/visual/ESP.java
@@ -4,6 +4,7 @@ import arsenic.event.bus.Listener;
 import arsenic.event.bus.annotations.EventLink;
 import arsenic.event.impl.EventRenderWorldLast;
 import arsenic.event.impl.EventTick;
+import arsenic.injection.accessor.IMixinRenderManager;
 import arsenic.module.Module;
 import arsenic.module.ModuleCategory;
 import arsenic.module.ModuleInfo;
@@ -13,6 +14,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import org.lwjgl.Sys;
@@ -25,25 +27,28 @@ public class ESP extends Module {
 
     @EventLink
     public final Listener<EventRenderWorldLast> renderWorldLast = event -> {
-        for(Entity entity : Minecraft.getMinecraft().theWorld.loadedEntityList) {
-            double x = entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * event.partialTicks;
-            double y = entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * event.partialTicks;
-            double z = entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * event.partialTicks;
+        for(EntityPlayer entity : Minecraft.getMinecraft().theWorld.playerEntities) {
+            if(entity == mc.thePlayer)
+                continue;
+            IMixinRenderManager renderManager = (IMixinRenderManager) mc.getRenderManager();
+            double x = (entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * event.partialTicks) - renderManager.getRenderPosX();
+            double y = (entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * event.partialTicks) - renderManager.getRenderPosY();
+            double z = (entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * event.partialTicks) - renderManager.getRenderPosZ();
             GlStateManager.pushMatrix();
             AxisAlignedBB axisalignedbb = entity.getEntityBoundingBox();
             AxisAlignedBB axisalignedbb1 = new AxisAlignedBB(axisalignedbb.minX - entity.posX + x, axisalignedbb.minY - entity.posY + y, axisalignedbb.minZ - entity.posZ + z, axisalignedbb.maxX - entity.posX + x, axisalignedbb.maxY - entity.posY + y, axisalignedbb.maxZ - entity.posZ + z);
-            GL11.glBlendFunc(770, 771);
-            GL11.glEnable(3042);
-            GL11.glDisable(3553);
-            GL11.glDisable(2929);
+            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+            GL11.glDisable(GL11.GL_TEXTURE_2D);
+            GL11.glDisable(GL11.GL_DEPTH_TEST);
+            GL11.glEnable(GL11.GL_BLEND);
             GL11.glDepthMask(false);
             GL11.glLineWidth(2.0F);
-            //RenderGlobal.drawSelectionBoundingBox(axisalignedbb1);
             RenderGlobal.drawOutlinedBoundingBox(axisalignedbb1, color.getColor(1), color.getColor(2), color.getColor(3), color.getColor(0));
-            GL11.glEnable(3553);
-            GL11.glEnable(2929);
+            GL11.glEnable(GL11.GL_TEXTURE_2D);
+            GL11.glEnable(GL11.GL_DEPTH_TEST);
+            GL11.glDisable(GL11.GL_BLEND);
             GL11.glDepthMask(true);
-            GL11.glDisable(3042);
+            GL11.glLineWidth(1.0F);
             GlStateManager.popMatrix();
         }
     };
