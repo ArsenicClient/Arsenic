086c58b2900a0aa6cccf1a1105b8bf8e88c671ac
K-v <58765519+K-ov@users.noreply.github.com>
2023-06-29 17:12:27 +1000

    rahHHHHHH

--- src/main/java/arsenic/module/ModuleCategory.java
+++ src/main/java/arsenic/module/ModuleCategory.java
@@ -8,16 +8,26 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public enum ModuleCategory implements IContainer<Module>, IContainable {
-    PLAYERS("Players"), WORLD("World"), CLIENT("Client"), GHOST("Ghost"), BLATANT("Blatant"), OTHER("Other"),
-    MOVEMENT("Movement");
+    PLAYERS,
+    WORLD,
+    CLIENT,
+    GHOST,
+    BLATANT,
+    OTHER,
+    MOVEMENT;
 
     private final String name;
 
-    ModuleCategory(String name) {
-        this.name = name;
+    private
+
+    ModuleCategory() {
+        name = name().substring(0, 1).toUpperCase() + name().substring(1);
     }
 
-    public String getName() { return name; }
+    @Override
+    public String getName() {
+        return name;
+    }
 
     @Override
     public Collection<Module> getContents() {
