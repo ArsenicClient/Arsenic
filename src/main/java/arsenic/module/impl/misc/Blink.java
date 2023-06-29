e6359e853976b5b0682cc6ded69c13f5be9ebd5b
K-v <58765519+K-ov@users.noreply.github.com>
2023-06-29 17:15:59 +1000

    blink improvement

--- src/main/java/arsenic/module/impl/misc/Blink.java
+++ src/main/java/arsenic/module/impl/misc/Blink.java
@@ -6,15 +6,32 @@ import arsenic.event.impl.EventPacket;
 import arsenic.module.Module;
 import arsenic.module.ModuleCategory;
 import arsenic.module.ModuleInfo;
+import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.C03PacketPlayer;
+
+import java.util.ArrayList;
 
 @ModuleInfo(name = "Blink",category = ModuleCategory.OTHER)
 public class Blink extends Module {
 
+    private ArrayList<Packet> packets = new ArrayList<>();
+
     @EventLink
     public final Listener<EventPacket.OutGoing> onPacket = event -> {
-        if (event.getPacket() instanceof C03PacketPlayer && mc.theWorld != null) {
-            event.setCancelled(true);
-        }
+        if(mc.theWorld == null)
+            return;
+        packets.add(event.getPacket());
     };
+
+    @Override
+    protected void onEnable() {
+        packets.clear();
+    }
+
+    @Override
+    protected void onDisable() {
+        for(Packet packet : packets) {
+            mc.getNetHandler().addToSendQueue(packet);
+        }
+    }
 }
