c0249441a20018f67e7e89c0ca578ec9ba3c85aa
K-v <58765519+K-ov@users.noreply.github.com>
2023-07-15 16:35:33 +1000

    mixin and event

--- src/main/java/arsenic/injection/mixin/MixinEntityLivingBase.java
+++ src/main/java/arsenic/injection/mixin/MixinEntityLivingBase.java
@@ -4,14 +4,12 @@ import arsenic.event.impl.EventJump;
 import arsenic.main.Arsenic;
 import arsenic.module.impl.movement.NoJumpDelay;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
-import net.minecraft.util.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityLivingBase.class)
@@ -23,12 +21,27 @@ public abstract class MixinEntityLivingBase extends MixinEntity {
     @Shadow
     private int jumpTicks;
 
+    private EventJump jumpEvent;
+
     @Inject(method = "jump", at = @At("HEAD"), cancellable = true)
-    private void jump2(CallbackInfo ci) {
-        EventJump event = new EventJump(this.rotationYaw, this.getJumpUpwardsMotion());
-        if (event.isCancelled())
+    private void jump(CallbackInfo ci) {
+        jumpEvent = new EventJump(this.rotationYaw, this.getJumpUpwardsMotion());
+        if (jumpEvent.isCancelled())
             ci.cancel();
     }
+
+    @Redirect(method = "jump", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityLivingBase;getJumpUpwardsMotion()F"))
+    private float upwardsMotion(EntityLivingBase instance) {
+        return jumpEvent.getMotion();
+    }
+
+    //ahh why does this give an error kys mc dev plugin
+    @ModifyVariable(method = "jump", at = @At(value = "STORE"), ordinal = 0)
+    public float yaw(float f) {
+        return jumpEvent.getYaw();
+    }
+
+
 
     @Inject(method = "onLivingUpdate", at = @At("HEAD"))
     private void headLiving(CallbackInfo callbackInfo) {
