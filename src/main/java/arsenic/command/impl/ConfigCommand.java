5a6b441a4dcda9e9b3641e88b1ffc68ed8e410bd
Cosmic <133380776+Cosmic-SC@users.noreply.github.com>
2023-09-24 00:39:37 +0400

    no idea what kind of shit this is but hey atleast it works
    crash fix and some misc things if i remember correctly

--- src/main/java/arsenic/command/impl/ConfigCommand.java
+++ src/main/java/arsenic/command/impl/ConfigCommand.java
@@ -6,9 +6,15 @@ import arsenic.config.ConfigManager;
 import arsenic.main.Arsenic;
 import arsenic.utils.minecraft.PlayerUtils;
 
-@CommandInfo(name = "config", args = { "config", "cfg" }, aliases = { "c" }, help = "lets you save/load configs", minArgs = 2)
-public class ConfigCommand extends Command {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@CommandInfo(name = "config", args = { "save/load/list", "config name" }, aliases = { "c" }, help = "helps you manipulate configs", minArgs = 1)
+public class ConfigCommand extends Command {
+    //don't scroll down if you wish to have living braincells
+    ArrayList<String> args = new ArrayList<>();
     @Override
     public void execute(String[] args) {
         ConfigManager configManager = Arsenic.getArsenic().getConfigManager();
@@ -20,11 +26,33 @@ public class ConfigCommand extends Command {
             } catch (NullPointerException e){
                 PlayerUtils.addWaterMarkedMessageToChat(args[1] + " does not exist");
             }
+        } else if (args[0].equalsIgnoreCase("list")){
+            PlayerUtils.addWaterMarkedMessageToChat("Configs that are available: ");
+            PlayerUtils.addWaterMarkedMessageToChat(configManager.getConfigList());
         } else if (args[0].equalsIgnoreCase("save") || args[0].equalsIgnoreCase("create")){
-            configManager.createConfig(args[1]);
-            PlayerUtils.addWaterMarkedMessageToChat( "created/saved " + args[1]);
+            try {
+                configManager.createConfig(args[1]);
+                PlayerUtils.addWaterMarkedMessageToChat("created/saved " + args[1]);
+            } catch (ArrayIndexOutOfBoundsException r){
+                PlayerUtils.addWaterMarkedMessageToChat("could not create/save a config with the name "+args[1]);
+            }
         } else {
             PlayerUtils.addWaterMarkedMessageToChat( args[0] + " is not a valid argument");
         }
+    }
+
+    @Override
+    protected List<String> getAutoComplete(String str, int arg, List<String> list) {
+        return arg == 0 ?
+                getClosestArg(str) :
+                Collections.singletonList(""); //idk bro dont ask me i never worked with such things
+    }
+    private List<String> getClosestArg(String name) { //idk wtf i did but atleast it works
+        if (args.isEmpty()){ //most scuffed thing you'll ever see BUT IT WORKS AAAAAA
+            args.add("list");
+            args.add("save");
+            args.add("load");
+        }
+        return args.stream().filter(c -> c.toLowerCase().startsWith(name.toLowerCase()) && c.length() > name.length()).map(String::toLowerCase).collect(Collectors.toList());
     }
 }
