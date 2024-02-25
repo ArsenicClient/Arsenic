378c30e63ff408757199e62b1644d48b5d9b1f6a
K-v <58765519+K-ov@users.noreply.github.com>
2024-02-25 17:45:39 +1100

    yippee removed code

--- src/main/java/arsenic/module/ModuleManager.java
+++ src/main/java/arsenic/module/ModuleManager.java
@@ -4,18 +4,7 @@ import arsenic.event.bus.Listener;
 import arsenic.event.bus.annotations.EventLink;
 import arsenic.event.impl.EventKey;
 import arsenic.main.Arsenic;
-import arsenic.module.impl.blatant.*;
-import arsenic.module.impl.client.*;
-import arsenic.module.impl.combat.*;
-import arsenic.module.impl.ghost.*;
-import arsenic.module.impl.misc.*;
-import arsenic.module.impl.misc.Sprint;
-import arsenic.module.impl.misc.chargetp.ChargeTp;
-import arsenic.module.impl.movement.*;
-import arsenic.module.impl.players.*;
-import arsenic.module.impl.visual.*;
-import arsenic.module.impl.visual.custommainmenu.CustomMainMenu;
-import arsenic.module.impl.world.*;
+import org.reflections.Reflections;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -23,54 +12,18 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
+import static org.reflections.scanners.Scanners.SubTypes;
+
 public class ModuleManager {
 
     private final Map<Class<? extends Module>, Module> modules = new HashMap<>();
 
     public final int initialize() {
-	if(modules.size() != 0)
-	    throw new RuntimeException("Double initialization of Module Manager.");
+        if(modules.size() != 0)
+            throw new RuntimeException("Double initialization of Module Manager.");
 
-        addModule(
-                ChargeTp.class,
-                FullBright.class,
-                Sprint.class,
-                InvMove.class,
-                NoJumpDelay.class,
-                HUD.class,
-                ClickGui.class,
-                ChestStealer.class,
-                FastPlace.class,
-                SmoothAim.class,
-                SafeWalk.class,
-                Velocity.class,
-                NoHurtCam.class,
-                Reach.class,
-                ESP.class,
-                Clicker.class,
-                ScaffoldTest.class,
-                HitBox.class,
-                AntiBot.class,
-                NoSlow.class,
-                Criticals.class,
-                Blink.class,
-                Flight.class,
-                CustomFOV.class,
-                CustomWorld.class,
-                InvManager.class,
-                NewHud.class,
-                Timer.class,
-                Aura.class,
-                AutoBlock.class,
-                Speed.class,
-                AutoSpacer.class,
-                TargetHud.class,
-                ScaffoldTest2.class,
-                AutoGG.class,
-                WTap.class,
-                ChargeTp.class,
-                FunnyModule.class,
-                CustomMainMenu.class);
+        Reflections reflections = new Reflections("arsenic.module");
+        reflections.get(SubTypes.of(Module.class).asClass()).forEach(module -> addModule((Class<? extends Module>) module));
 
         Arsenic.getInstance().getEventManager().subscribe(this);
         return modules.size();
@@ -102,24 +55,20 @@ public class ModuleManager {
     public final Listener<EventKey> onKeyPress = event -> {
         AtomicBoolean saveConfig = new AtomicBoolean(false); // for eff
 
-	getModules().stream().filter(m -> m.getKeybind() == event.getKeycode())
-	    .forEach(m -> {
-		m.setEnabled(!m.isEnabled());
-		saveConfig.set(true);
-	    });
+        getModules().stream().filter(m -> m.getKeybind() == event.getKeycode())
+                .forEach(m -> {
+                    m.setEnabled(!m.isEnabled());
+                    saveConfig.set(true);
+                });
 
         if (saveConfig.get()) { Arsenic.getArsenic().getConfigManager().saveConfig(); }
     };
 
-    private void addModule(Class<? extends Module>... moduleClassArray) {
-        for(Class<? extends Module> moduleClass : moduleClassArray) {
-            try {
-                Module module = moduleClass.newInstance();
-                module.registerProperties();
-                modules.put(moduleClass, module);
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-        }
+    private void addModule(Class<? extends Module> moduleClass) {
+        try {
+            Module module = moduleClass.newInstance();
+            module.registerProperties();
+            modules.put(moduleClass, module);
+        } catch (Exception e) {e.printStackTrace();}
     }
 }
