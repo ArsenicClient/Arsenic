b766db45fafe6c24eebfcc4eee4813a6586f4bb2
Cosmic <133380776+Cosmic-SC@users.noreply.github.com>
2024-07-03 00:16:42 +0400

    Clicker recode

--- src/main/java/arsenic/injection/accessor/IMixinMinecraft.java
+++ src/main/java/arsenic/injection/accessor/IMixinMinecraft.java
@@ -4,9 +4,13 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.util.Timer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(Minecraft.class)
 public interface IMixinMinecraft {
     @Accessor
     Timer getTimer();
+
+    @Invoker("clickMouse")
+    void leftClick();
 }
