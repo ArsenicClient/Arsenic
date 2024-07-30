26a2836e68e655f9d3c8d979f8579467d7eb791d
azureskylines <kay@deviance.lol>
2024-07-30 18:23:56 +0200

    Update MixinGuiIngame.java

--- src/main/java/arsenic/injection/mixin/MixinGuiIngame.java
+++ src/main/java/arsenic/injection/mixin/MixinGuiIngame.java
@@ -9,20 +9,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import arsenic.event.impl.EventRender2D;
 import arsenic.main.Arsenic;
 import net.minecraft.client.gui.GuiIngame;
+import net.minecraft.client.main.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
 
 @Mixin(priority = 1111, value = GuiIngame.class)
 public class MixinGuiIngame {
 
-    @Inject(method = "renderTooltip", at = @At("RETURN"))
-    private void renderTooltip(ScaledResolution sr, float partialTicks, CallbackInfo ci) {
+    @Inject(method = "renderGameOverlay", at = @At("HEAD"))
+    private void renderGameOverlay(float partialTicks, CallbackInfo ci) {
         if (!System.getProperty("os.name").toLowerCase().contains("mac")) {
             PostProcessing postProcessing = Arsenic.getArsenic().getModuleManager().getModuleByClass(PostProcessing.class);
             if (postProcessing.isEnabled()) {
                 postProcessing.blurScreen();
             }
         }
-        Arsenic.getInstance().getEventManager().post(new EventRender2D(partialTicks, sr));
+        Arsenic.getInstance().getEventManager().post(new EventRender2D(partialTicks, new ScaledResolution(Minecraft.getMinecraft())));
     }
 
 }
