5f9ef6301d03fd69d57611fe87fbfd25355a2fcf
TejasLamba2006 <tejas22feb@gmail.com>
2024-07-31 12:05:35 +0530

    Fix HUD

--- src/main/java/arsenic/injection/mixin/MixinGuiIngame.java
+++ src/main/java/arsenic/injection/mixin/MixinGuiIngame.java
@@ -1,7 +1,6 @@
 package arsenic.injection.mixin;
 
 import arsenic.module.impl.client.PostProcessing;
-import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,15 +13,15 @@ import net.minecraft.client.gui.ScaledResolution;
 @Mixin(priority = 1111, value = GuiIngame.class)
 public class MixinGuiIngame {
 
-    @Inject(method = "renderGameOverlay", at = @At("HEAD"))
-    private void renderGameOverlay(float partialTicks, CallbackInfo ci) {
+    @Inject(method = "renderTooltip", at = @At("RETURN"))
+    private void renderTooltip(ScaledResolution sr, float partialTicks, CallbackInfo ci) {
         if (!System.getProperty("os.name").toLowerCase().contains("mac")) {
             PostProcessing postProcessing = Arsenic.getArsenic().getModuleManager().getModuleByClass(PostProcessing.class);
             if (postProcessing.isEnabled()) {
                 postProcessing.blurScreen();
             }
         }
-        Arsenic.getInstance().getEventManager().post(new EventRender2D(partialTicks, new ScaledResolution(Minecraft.getMinecraft())));
+        Arsenic.getInstance().getEventManager().post(new EventRender2D(partialTicks, sr));
     }
 
 }
