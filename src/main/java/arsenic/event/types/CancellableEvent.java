b308dbe2cd4b7b5f1382862c88e5c4c05de6d764
TejasLamba2006 <tejas22feb@gmail.com>
2024-10-13 18:56:54 +0530

    Implements #15

--- src/main/java/arsenic/event/types/CancellableEvent.java
+++ src/main/java/arsenic/event/types/CancellableEvent.java
@@ -1,12 +1,14 @@
 package arsenic.event.types;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class CancellableEvent implements Event {
 
-    private boolean cancelled;
+    AtomicBoolean cancelled = new AtomicBoolean(false);
 
-    public boolean isCancelled() { return cancelled; }
+    public boolean isCancelled() { return this.cancelled.get(); }
 
-    public void setCancelled(boolean cancelled) { this.cancelled = cancelled; }
+    public void setCancelled(boolean cancelled) { this.cancelled.set(cancelled); }
 
     public void cancel() {
         setCancelled(true);
