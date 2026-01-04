ec1df4213b4a4b3bac3f5e90fb0c960f63bd3468
JaxxingIt <jacobcoiler708@gmail.com>
2026-01-04 12:26:05 +1100

    Better Wtap?

--- src/main/java/arsenic/module/impl/combat/WTap.java
+++ src/main/java/arsenic/module/impl/combat/WTap.java
@@ -15,25 +15,22 @@ import net.minecraft.entity.EntityLivingBase;
 @ModuleInfo(name = "WTap",category = ModuleCategory.GHOST)
 public class WTap extends Module {
     public final DoubleProperty hurtTime = new DoubleProperty("HurtTime",new DoubleValue(1,10,10,1));
-    boolean stop;
+    public EntityLivingBase target;
 
     @EventLink
     public final Listener<EventAttack> eventAttackListener = event -> {
         if (event.getTarget() != null && event.getTarget() instanceof EntityLivingBase) {
-            if (((EntityLivingBase) event.getTarget()).hurtTime == hurtTime.getValue().getInput()){
-                stop = true;
-            }
+            target = (EntityLivingBase) event.getTarget();
         }
     };
 
     @RequiresPlayer
     @EventLink
     public final Listener<EventMovementInput> movementInputListener = event -> {
-        if (mc.thePlayer.isSprinting()) {
-            if (stop) {
-                event.setSpeed(0);
-                stop = false;
-            }
+        if(target == null) return;
+        if (mc.thePlayer.isSprinting() && target.hurtTime == hurtTime.getValue().getInput()) {
+            event.setSpeed(0);
+            target = null;
         }
     };
 }
