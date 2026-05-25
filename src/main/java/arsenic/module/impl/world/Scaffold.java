76bae3c9b57d91a579a9d4fd23169bb94c5bf5f9
JaxxingIt <jaxtaylor07@gmail.com>
2026-05-25 11:11:42 +1000

    only place blocks if we need to

--- src/main/java/arsenic/module/impl/world/Scaffold.java
+++ src/main/java/arsenic/module/impl/world/Scaffold.java
@@ -20,14 +20,20 @@ import arsenic.utils.minecraft.ScaffoldUtil;
 import arsenic.utils.render.RenderUtils;
 import arsenic.utils.rotations.RotationUtils;
 import ibxm.Player;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.MathHelper;
 import net.minecraft.util.MovingObjectPosition;
 import org.lwjgl.input.Keyboard;
+
+import static arsenic.utils.rotations.RotationUtils.clamp;
+import static arsenic.utils.rotations.RotationUtils.patchGCD;
 
 @ModuleInfo(name = "Scaffold", category = ModuleCategory.PLAYER)
 public class Scaffold extends Module {
@@ -50,7 +56,7 @@ public class Scaffold extends Module {
     @RequiresPlayer
     @EventLink
     public final Listener<EventSilentRotation> eventSilentRotationListener = event -> {
-        if (ScaffoldUtil.getBlockData() != null) {
+        if (ScaffoldUtil.getBlockData() != null && willFallNextTick()) {
             lastBlockData = ScaffoldUtil.getBlockData();
             rots = RotationUtils.getRotations(ScaffoldUtil.getBlockData().getPosition());
         }
@@ -100,6 +106,57 @@ public class Scaffold extends Module {
         );
 
         mc.thePlayer.swingItem();
+    }
+
+    public boolean willFallNextTick() {
+        EntityPlayerSP player = mc.thePlayer;
+        if (!player.onGround)
+            return true;
+
+        double nextX = player.posX + player.motionX * 1.2;
+        double nextZ = player.posZ + player.motionZ * 1.2;
+        double nextY = player.posY + player.motionY * 1.2;
+
+        double halfWidth = 0.29;
+
+        double[][] checkPoints = {
+                {nextX,             nextZ},
+                {nextX + halfWidth, nextZ + halfWidth},
+                {nextX - halfWidth, nextZ + halfWidth},
+                {nextX + halfWidth, nextZ - halfWidth},
+                {nextX - halfWidth, nextZ - halfWidth},
+        };
+
+        for (double[] point : checkPoints) {
+            BlockPos groundPos = new BlockPos(
+                    MathHelper.floor_double(point[0]),
+                    MathHelper.floor_double(nextY) - 1, // block directly beneath feet
+                    MathHelper.floor_double(point[1])
+            );
+
+            Block block = mc.theWorld.getBlockState(groundPos).getBlock();
+
+            // If any corner has a solid block beneath it, player won't fall
+            if (block.getMaterial() != Material.air) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static float[] getRotations(final BlockPos blockPos) {
+        final double x = blockPos.getX() + 0.45 - mc.thePlayer.posX;
+        final double y = blockPos.getY() + 0.45 - (mc.thePlayer.posY + mc.thePlayer.getEyeHeight());
+        final double z = blockPos.getZ() + 0.45 - mc.thePlayer.posZ;
+        float[] targetRots = new float[]{mc.thePlayer.rotationYaw + MathHelper.wrapAngleTo180_float((float) (Math.atan2(z, x) * 57.295780181884766) - 90.0f - mc.thePlayer.rotationYaw), clamp(mc.thePlayer.rotationPitch + MathHelper.wrapAngleTo180_float((float) (-(Math.atan2(y, MathHelper.sqrt_double(x * x + z * z)) * 57.295780181884766)) - mc.thePlayer.rotationPitch))};
+
+        float currentYaw = Arsenic.getArsenic().getSilentRotationManager().yaw;
+        float currentPitch = Arsenic.getArsenic().getSilentRotationManager().pitch;
+
+        float[] lastRots = new float[]{currentYaw, currentPitch};
+        float[] fixedRots = patchGCD(lastRots, targetRots);
+        return fixedRots;
     }
 
 
