99a3552847770a7667ccd3750461fb386e6c821c
JaxxingIt <jaxtaylor07@gmail.com>
2026-05-28 23:23:28 +1000

    make the jar smaller

--- src/main/java/arsenic/utils/font/Fonts.java
+++ src/main/java/arsenic/utils/font/Fonts.java
@@ -10,11 +10,8 @@ import org.jetbrains.annotations.Nullable;
 
 public class Fonts {
 
-    public final TTFontRenderer FR = new TTFontRenderer(getFontFromLocation("font.ttf", 21), true, true);
-    public final TTFontRenderer MEDIUM_FR = new TTFontRenderer(getFontFromLocation("font.ttf", 20), true, true);
-    public final TTFontRenderer SMALL_FR = new TTFontRenderer(getFontFromLocation("font.ttf", 18), true, true);
+
     public final TTFontRenderer Comfortaa = new TTFontRenderer(getFontFromLocation("comfortaa.ttf", 17), true, true);
-    public final TTFontRenderer ComfortaaMedium = new TTFontRenderer(getFontFromLocation("comfortaa.ttf", 18), true, true);
     public final TTFontRenderer Icon = new TTFontRenderer(getFontFromLocation("icon.ttf", 20), true, true);
 
     public void initTextures() {
